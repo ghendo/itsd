6bb9527a70f09cd07aee4bbde724e0f7bc57cb2f
Graeme Henderson <ghendo@me.com>
2017-11-27 10:10:42 +1100

    COMMIT MESSAGE 2

---
+++ itsd_WipeDrive/itsd_WipeDrive.Data/ReportDataAccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace itsd_WipeDrive.Data
+{
+    public class ReportDataAccess
+    {
+        public void ReadReport()
+        {
+            //create connection
+          
+        }
+    }
+}
